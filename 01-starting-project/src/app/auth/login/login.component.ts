df03fe3a1ea08c797a0a9b990c295e1914706bac
Raluca <spatarularisaraluca@gmail.com>
2025-01-12 05:25:16 +0200

    Add validation to login form inputs

--- 01-starting-project/src/app/auth/login/login.component.ts
+++ 01-starting-project/src/app/auth/login/login.component.ts
@@ -10,6 +10,10 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
   onSubmit(formData: NgForm) {
+    if(formData.form.valid){
+      return;
+    }
+
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
     console.log(enteredEmail, enteredPassword);
